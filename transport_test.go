2064952832bc222c6a662a470af56ce8a50cebc5
eslizn <eslizn@gmail.com>
2020-06-01 01:27:43 +0800

    add tcp test case

--- transport_test.go
+++ transport_test.go
@@ -3,6 +3,9 @@ package jsonrpc
 import (
 	"context"
 	"fmt"
+	"net"
+	"net/url"
+	"os"
 	"testing"
 	"time"
 
@@ -62,6 +65,89 @@ func TestTransport(t *testing.T) {
 			}
 		} else {
 			future := rafts[0].AddVoter(raft.ServerID(fmt.Sprint(k)), raft.ServerAddress(addrs[k]), 0, 0)
+			if future.Error() != nil {
+				t.Logf("AddVoter(%d, %s) error: %s\n", k, addrs[k], future.Error())
+			}
+
+		}
+	}
+	//fmt.Println("start check")
+	//ready := false
+	//for !ready {
+	//	select {
+	//	case <-time.NewTimer(time.Second).C:
+	//		for k := range rafts {
+	//			if rafts[k].State() == raft.Leader {
+	//				ready = true
+	//				break
+	//			}
+	//		}
+	//	case <-ctx.Done():
+	//		return
+	//	}
+	//}
+	for k := range rafts {
+		t.Logf("%d %s ---> leader: %s\n", k, rafts[k].State(), rafts[k].Leader())
+		t.Logf("%s %+v\n", rafts[k].State(), rafts[k].Apply([]byte("test"), time.Second).Error())
+	}
+	time.Sleep(5 * time.Second)
+	t.Logf("%+v\n", rafts[0].GetConfiguration().Configuration().Servers)
+
+	//time.Sleep(time.Minute)
+}
+
+func TestTCPTransport(t *testing.T) {
+	var (
+		addrs = []string{
+			"tcp://127.0.0.1:10001/",
+			"tcp://127.0.0.1:10002/",
+			"tcp://127.0.0.1:10003/",
+		}
+		rafts       = make([]*raft.Raft, len(addrs))
+		ctx, cancel = context.WithTimeout(context.Background(), 30*time.Second)
+	)
+	defer cancel()
+	for k := range addrs {
+		var (
+			parse, _   = url.Parse(addrs[k])
+			addr, _    = net.ResolveTCPAddr(parse.Scheme, parse.Host)
+			config     = raft.DefaultConfig()
+			fsm        = &raft.MockFSM{}
+			logs       = raft.NewInmemStore()
+			stable     = raft.NewInmemStore()
+			snapshot   = raft.NewInmemSnapshotStore()
+			trans, err = raft.NewTCPTransport(parse.Host, addr, 8, 0, os.Stdout)
+		)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		config.LogLevel = "ERROR" //"WARN"
+		config.LocalID = raft.ServerID(fmt.Sprint(k))
+		rafts[k], err = raft.NewRaft(config, fsm, logs, stable, snapshot, trans)
+		if err != nil {
+			t.Log(err)
+			return
+		}
+		if k == 0 {
+			rafts[k].BootstrapCluster(raft.Configuration{
+				Servers: []raft.Server{
+					{
+						ID:      config.LocalID,
+						Address: raft.ServerAddress(parse.Host),
+					},
+				},
+			})
+			for rafts[k].State() != raft.Leader {
+				select {
+				case <-ctx.Done():
+					return
+				case <-time.NewTimer(time.Second).C:
+					continue
+				}
+			}
+		} else {
+			future := rafts[0].AddVoter(raft.ServerID(fmt.Sprint(k)), raft.ServerAddress(parse.Host), 0, 0)
 			if future.Error() != nil {
 				t.Logf("AddVoter(%d, %s) error: %s\n", k, addrs[k], future.Error())
 			}
